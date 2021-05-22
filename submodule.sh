4a8bf8ec565bfaec21965bbcba4712f9b3145e88
atefganm <atef_anim_1974@yahoo.com>
2021-05-22 09:33:33 +0200

    update

--- submodule.sh
+++ submodule.sh
@@ -4,55 +4,11 @@ echo "This is only for MastaG, Persian Prince and Hains with push access"
 echo ""
 echo "Stage 1: git pull for new changes"
 echo ""
-cd meta-amlogic
-git checkout develop
-git pull
-cd ..
-cd meta-azbox
-git checkout develop
-git pull
-cd ..
-cd meta-ceryon
-git checkout develop
-git pull
-cd ..
-cd meta-dags
-git checkout develop
-git pull
-cd ..
-cd meta-dinobot
-git checkout develop
-git pull
-cd ..
 cd meta-dream
 git checkout develop
 git pull
 cd ..
-cd meta-entwopia
-git checkout develop
-git pull
-cd ..
 cd meta-fulan
-git checkout develop
-git pull
-cd ..
-cd meta-ini
-git checkout develop
-git pull
-cd ..
-cd meta-octagon
-git checkout develop
-git pull
-cd ..
-cd meta-odin
-git checkout develop
-git pull
-cd ..
-cd meta-raspberrypi
-git checkout develop
-git pull
-cd ..
-cd meta-xcore
 git checkout develop
 git pull
 cd ..
